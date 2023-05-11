0a263287e4fe22c6973d75cfb82b314038b39f71
maiko.trindade <maikocharles@gmail.com>
2023-05-10 23:59:45 -0600

    initial impl of smart contract integration

--- index.ts
+++ index.ts
@@ -1,12 +1,42 @@
 #!/usr/bin/env ts-node
 /* eslint-disable import/first */
 require('dotenv').config();
+const axios = require('axios');
 
-const { Network, Alchemy, Wallet, Utils } = require("alchemy-sdk");
-const { ALCHEMY_KEY, ACCOUNT_TEST_1_ADDRESS, ACCOUNT_TEST_1_PRIVATE_KEY, ACCOUNT_TEST_2_ADDRESS} = process.env;
+const { Network, Alchemy, Wallet, Utils} = require("alchemy-sdk");
+const { ethers } = require("ethers");
+const { ALCHEMY_KEY, OWNER_ACCOUNT_PRIVATE_KEY, TEST_ADDRESS_TO } = process.env;
 
 const settings = {
     apiKey: ALCHEMY_KEY,
-    network: Network.ETH_GOERLI,
+    network: Network.ETH_SEPOLIA,
 };
 const alchemy = new Alchemy(settings);
+
+const signer = new Wallet(OWNER_ACCOUNT_PRIVATE_KEY, alchemy);
+
+// TODO fetch data from other repo
+const contractAddress = "https://raw.githubusercontent.com/maikotrindade/nomad-token/main/contract/NomadBadgeAddress.json"
+const contractABI = "https://raw.githubusercontent.com/maikotrindade/nomad-token/main/contract/NomadBadge.json"
+
+async function main() {
+    try {
+        const addressResponse = await axios.get(contractAddress);
+        const abiResponse = await axios.get(contractABI);
+        
+        const contract = new ethers.Contract(addressResponse.data.Contract, abiResponse.data.abi, signer);
+
+        const run = await contract.getPoints(0);
+
+        // const points = await contract.getPoints(0);
+        // console.log(points);
+
+        // const runRewardProcess = await contract.runRewardProcess(TEST_ADDRESS_TO)
+        // console.log(runRewardProcess);
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    
+    main();
